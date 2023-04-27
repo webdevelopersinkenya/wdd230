bf27c1d5125a1f82933b00f5c369e7a007e7c653
webdevelopersinkenya <mir22006@byui.edu>
2023-04-27 12:36:12 +0300

    lesson2

---
+++ scripts/design.js
@@ -0,0 +1,10 @@
+<div class="image-gallery">
+   <div class="image-container">
+      <img src="image1.jpg" alt="Image 1">
+      <p class="image-description">Description for Image 1</p>
+   </div>
+   <div class="image-container">
+      <img src="image2.jpg" alt="Image 2">
+      <p class="image-description">Description for Image 2</p>
+   </div>
+</div>
